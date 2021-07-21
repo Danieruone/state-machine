e5cdbd585f780ac2d424ef7d72de4830d9aa8f77
Daniel <danieruone@gmail.com>
2021-07-20 21:22:05 -0500

    state machine logic

--- src/state-machine.js
+++ src/state-machine.js
@@ -1,17 +1,72 @@
 class Machine {
   alphabet = ["f", "i", "n", "d"];
-  array = [];
+  input = [];
+  validator = [];
+
   constructor(string) {
-    this.array = string.toLowerCase().split("");
+    this.input = string.toLowerCase().split("");
   }
+
   startMachine() {
-    // if (!this.validateString()) return console.log("String no válido");
-    return this.validateString();
+    if (!this.validateString()) return console.log("String NO válido");
+    return this.startStates();
   }
+
   validateString() {
-    return this.array.every((value) =>
+    return this.input.every((value) =>
       this.alphabet.some((letter) => value === letter)
     );
+  }
+
+  startStates() {
+    this.input.map((letter) => {
+      switch (letter) {
+        case "f":
+          this.q0(letter);
+          break;
+        case "i":
+          this.q1(letter);
+          break;
+        case "n":
+          this.q2(letter);
+          break;
+        case "d":
+          this.q3(letter);
+          break;
+      }
+    });
+    if (this.validateResult()) {
+      console.log("String válido");
+      return true;
+    } else {
+      console.log("String NO válido");
+      return false;
+    }
+  }
+
+  q0(letter) {
+    console.log(`Estado actual q0 con valor ${letter}`);
+    if (!this.validator.includes("f")) this.validator.push(letter);
+  }
+  q1(letter) {
+    console.log(`Estado actual q1 con valor ${letter}`);
+    if (!this.validator.includes("i")) this.validator.push(letter);
+  }
+  q2(letter) {
+    console.log(`Estado actual q2 con valor ${letter}`);
+    if (!this.validator.includes("n")) this.validator.push(letter);
+  }
+  q3(letter) {
+    console.log(`Estado actual q3 con valor ${letter}`);
+    if (!this.validator.includes("d")) this.validator.push(letter);
+  }
+
+  validateResult() {
+    let valid = true;
+    this.alphabet.map((letter) => {
+      this.validator.includes(letter) ? null : (valid = false);
+    });
+    return valid;
   }
 }
 
--- tests/state-machine.test.js
+++ tests/state-machine.test.js
@@ -3,11 +3,26 @@ const Machine = require("../src/state-machine");
 describe("state machine tests", () => {
   test("get a string 'Fins' in the class and validate it", () => {
     let machine = new Machine("Fins");
-    expect(machine.startMachine()).toBe(false);
+    expect(machine.startMachine()).toBe(undefined);
   });
 
   test("get a string 'fffiiinndd' in the class and validate it", () => {
     let machine = new Machine("fffiiinndd");
+    expect(machine.startMachine()).toBe(true);
+  });
+
+  test("get a string 'fffiiinn' in the class and validate it", () => {
+    let machine = new Machine("fffiiinn");
+    expect(machine.startMachine()).toBe(false);
+  });
+
+  test("get a string 'iiinnd' in the class and validate it", () => {
+    let machine = new Machine("iiinnd");
+    expect(machine.startMachine()).toBe(false);
+  });
+
+  test("get a string 'innfnd' in the class and validate it", () => {
+    let machine = new Machine("innfnd");
     expect(machine.startMachine()).toBe(true);
   });
 });
